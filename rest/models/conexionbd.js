b10afb8176bf948025724df464a57bf52de34d89
z1ber Alien <ron0adal@gmail.com>
2019-07-22 01:43:01 -0600

    Cambie el parametro para login y el usuario para la conexion a la bd

--- rest/models/conexionbd.js
+++ rest/models/conexionbd.js
@@ -6,7 +6,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'mysql',
+    password : 'mysql123API',
     database : 'horario_cds'
 });
 
