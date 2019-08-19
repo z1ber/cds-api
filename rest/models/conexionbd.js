1f4d073c6daf28c8a25ea1b5a7a3a59339b90bd2
Ronald Quevedo <ron0adal@gmail.com>
2019-08-19 14:08:23 -0600

    Cambiando las variables para conectar a la base de datos

--- rest/models/conexionbd.js
+++ rest/models/conexionbd.js
@@ -4,10 +4,10 @@ var mysql = require('mysql');
 
 //local mysql db connection
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'b3642c0a7d88bb',
-    password : 'ba157c25',
-    database : 'horario_cds'
+    host     : 'bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user     : 'zd3zf9lvldthj1e5',
+    password : 'be4t7h6i3zitpa13',
+    database : 'ekvzet0m6f3ls5hn'
 });
 
 connection.connect(function(err) {
